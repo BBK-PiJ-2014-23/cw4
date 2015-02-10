873de4a0e90c871d6e0db42ac8f38a1edbf2253a
unknown <stefanm@LONWRK08546.eidos.com>
2015-02-10 11:33:42 +0000

    Created initial tester for class 'ContactImpl'.

---
+++ ContactTester.java
@@ -0,0 +1,34 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class ContactTester.
+ *
+ * @author  Stefan E. Mayer
+ * @version 1.0
+ */
+public class ContactTester {
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
