3b614eae8e28e033dca99ce63a28b6dd57d408ee
Stefan E. Mayer <stefan.mayer.80@gmail.com>
2015-03-21 12:59:11 +0000

    Added some annotations to Javadocs.

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -2,11 +2,15 @@ import java.util.*;
 import java.io.Serializable;
 /**
  * A meeting to be held in the future
+ * 
+ * @author Stefan E. Mayer
+ * @version 1.0
  */
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable {
     /**
      * Constructor for a future meeting.
-     *
+     * 
+     * @param id the id of the meeting
      * @param contacts a list of contacts that will participate in the meeting
      * @param date the date on which the meeting will take place
      */
