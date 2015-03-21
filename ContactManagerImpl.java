e8c315f61d3d0616026390f057a8ea516f325cb6
Stefan E. Mayer <stefan.mayer.80@gmail.com>
2015-03-21 12:17:30 +0000

    Implemented Javadoc inheritance for one method.

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -42,13 +42,7 @@ public class ContactManagerImpl implements ContactManager {
     }
 
     /**
-     * Add a new meeting to be held in the future.
-     *
-     * @param contacts a list of contacts that will participate in the meeting
-     * @param date the date on which the meeting will take place
-     * @return the ID for the meeting
-     * @throws IllegalArgumentException if the meeting is set for a time in the past,
-     * of if any contact is unknown / non-existent
+     * {@inheritDoc}
      */
     @Override
     public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
@@ -71,7 +65,7 @@ public class ContactManagerImpl implements ContactManager {
      *
      * @param id the ID for the meeting
      * @return the meeting with the requested ID, or null if it there is none.
-     * @throws IllegalArgumentException if there is a meeting with that ID happening in the future
+     * @throws IllegalArgumentException if there is a meeting with that ID happening in the future.
      */
     @Override
     public PastMeeting getPastMeeting(int id) {
