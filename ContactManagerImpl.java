99e7d8174cb083b1e2302e20d3c96db49ee3475b
Stefan E. Mayer <stefan.mayer.80@gmail.com>
2015-03-21 12:32:36 +0000

    Implemented Javadoc inheritance for a method.

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -213,14 +213,7 @@ public class ContactManagerImpl implements ContactManager {
     }
 
     /**
-     * Create a new record for a meeting that took place in the past.
-     *
-     * @param contacts a list of participants
-     * @param date the date on which the meeting took place
-     * @param text messages to be added about the meeting.
-     * @throws IllegalArgumentException if the list of contacts is
-     * empty, or any of the contacts does not exist
-     * @throws NullPointerException if any of the arguments is null
+     * {@inheritDoc}
      */
     @Override
     public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) {
